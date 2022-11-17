0f0e57f7d1028ea565b89bc19c9df2ed223f0f5c
Sven Balnojan <43072233+sbalnojan@users.noreply.github.com>
2022-11-17 13:17:36 +0000

    change default duckdb schema

--- codespaces_tutorial/select_duckdb.py
+++ codespaces_tutorial/select_duckdb.py
@@ -3,5 +3,5 @@ import duckdb
 # to use a database file (not shared between processes)
 con = duckdb.connect(database=output/my.duckdb', read_only=True)
 
-con.execute("SELECT * FROM public.raw_customers")
+con.execute("SELECT * FROM raw.raw_customers")
 print(con.fetchall())
