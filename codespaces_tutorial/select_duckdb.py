5b075edb954fbbbed745a22492b8033e3e324907
Sven Balnojan <43072233+sbalnojan@users.noreply.github.com>
2022-11-17 12:59:30 +0000

    add tut folder

---
+++ codespaces_tutorial/select_duckdb.py
@@ -0,0 +1,7 @@
+import duckdb
+
+# to use a database file (not shared between processes)
+con = duckdb.connect(database=output/my.duckdb', read_only=True)
+
+con.execute("SELECT * FROM public.raw_customers")
+print(con.fetchall())
